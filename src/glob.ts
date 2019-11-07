b1872ed25cb15ce2bdae246995b8a57d00e039dd
Alex Young <alex.young@hive.hr>
2019-11-07 23:39:29 +0000

    Add bin script to glob test ids

---
+++ src/glob.ts
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import * as fs from "fs-extra";
+import * as glob from "glob";
+import * as path from "path";
+import commander from "commander";
+
+const program = new commander.Command("glob-test-ids");
+
+const packageJson = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "..", "package.json")).toString()
+);
+
+program.version(packageJson.version);
+
+program
+    .requiredOption("-i, --ids-location <location>", "glob pattern of id files")
+    .requiredOption("-o, --output <output>", "Output directory")
+    .option("-f, --file", "Filename to output", "master.json");
+
+program.parse(process.argv);
+
+function globTestIds() {
+    try {
+        const allIds = glob
+            .sync(path.join(process.cwd(), program.idsLocation))
+            .map(filename => ({
+                filename,
+                file: fs.readFileSync(filename, "utf8")
+            }))
+            .map(({ filename, file }) => ({
+                filename,
+                file: JSON.parse(file) as string[]
+            }))
+            .reduce(
+                (ids, { filename, file }) => {
+                    file.forEach(testId => {
+                        if (testId in ids) {
+                            throw new Error(
+                                `Duplicate test id ${testId} in ${filename}`
+                            );
+                        }
+                        ids[testId] = testId;
+                    });
+                    return ids;
+                },
+                {} as Record<string, string>
+            );
+
+        fs.mkdirpSync(path.join(process.cwd(), program.output));
+
+        const outFile = path.join(process.cwd(), program.output, program.file);
+
+        fs.writeFileSync(outFile, JSON.stringify(allIds, null, 2));
+        console.log(
+            `${Object.keys(allIds).length} test ids written to ${outFile}`
+        );
+        process.exit(0);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+globTestIds();
